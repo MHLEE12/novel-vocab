d05274923f29352111162b8b7e2da2b70d120cd6
MHLEE12 <coolmh12@gmail.com>
2025-03-17 18:06:25 +0900

    Init: springboot project

---
+++ src/main/java/com/novelvocab/novel_vocab/NovelVocabApplication.java
@@ -0,0 +1,13 @@
+package com.novelvocab.novel_vocab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelVocabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelVocabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novelvocab/novel_vocab/NovelVocabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novelvocab.novel_vocab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelVocabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
